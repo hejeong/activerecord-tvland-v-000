232144b0fa75becab6f56f4ea134e3074bc5cbf9
Jonathan Hong <jonhong777@gmail.com>
2018-08-08 22:02:37 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,8 +7,8 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    roles = [] 
-    self.characters.each do |char| 
+    roles = []
+    self.characters.each do |char|
       roles << char.name
       roles << char.show.name
     end
