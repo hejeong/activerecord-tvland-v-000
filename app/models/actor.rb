39205867f4b64c2aad1580f566a29c50acc36136
Jonathan Hong <jonhong777@gmail.com>
2018-08-08 22:01:50 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     roles = self.characters.map {|char| char.name}
-    shows = roles.map {|char| char.show.name}
+    shows = self.characters.map {|char| char.show.name}
     roles.concat(shows)
   end
 end
